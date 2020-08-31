6e513ca7086cb8963fcba9baddd2ec8973f24450
Amjad Odeh <aao001056485@gmail.com>
2020-08-31 16:56:36 -0400

    user can now navigate through the options of the questions with arrow keys

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ const STORE = {
 // These functions return HTML templates
 
 function generateTemplate(qN) {
-  return '<div class="main-parent"><ul class="question-and-score"><li id="question-number">Question Number: ' + (qN+1) + '/5</li><li id="score">Score: ' + (STORE.score) + '/5</li></ul><img src="' + (STORE.questions[qN].gif) + '" alt="slow motion skateboarding trick demo"><div class="box"><div class="correct"><p>Thats correct! This is a ' + (STORE.questions[STORE.questionNumber].correctAnswer) + '.<p/></div><div class="incorrect"><p>Thats incorrect, this is a ' + (STORE.questions[STORE.questionNumber].correctAnswer) + '.<p/></div><p class="q">What is the name of this trick?</p><form class="options"><input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.a) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option a" aria-pressed="false" class="red-bg option"><input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.b) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option b" aria-pressed="false" class="orange-bg option"></form><form class="options"><input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.c) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option c" aria-pressed="false" class="green-bg option"><input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.d) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option d" aria-pressed="false" class="blue-bg option"></form><button type="button" id="submit" class="submit">Submit</button><button type="button" id="next" class="next">Next</button></div></div>';
+  return '<div class="main-parent"><ul class="question-and-score"><li id="question-number">Question Number: ' + (qN+1) + '/5</li><li id="score">Score: ' + (STORE.score) + '/5</li></ul><img src="' + (STORE.questions[qN].gif) + '" alt="slow motion skateboarding trick demo"><div class="box"><div class="correct"><p>Thats correct! This is a ' + (STORE.questions[STORE.questionNumber].correctAnswer) + '.<p/></div><div class="incorrect"><p>Thats incorrect, this is a ' + (STORE.questions[STORE.questionNumber].correctAnswer) + '.<p/></div><p class="q">What is the name of this trick?</p><form class="options"><input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.a) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option a" aria-pressed="false" class="red-bg option arrowNav"><input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.b) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option b" aria-pressed="false" class="orange-bg option arrowNav"></form><form class="options"><input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.c) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option c" aria-pressed="false" class="green-bg option arrowNav"><input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.d) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option d" aria-pressed="false" class="blue-bg option arrowNav"></form><button type="button" id="submit" class="submit arrowNav">Submit</button><button type="button" id="next" class="next arrowNav">Next</button></div></div>';
 
   /*
 
@@ -111,16 +111,16 @@ function generateTemplate(qN) {
 
     <p class="q">What is the name of this trick?</p>
     <form class="options">
-      <input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.a) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option a" aria-pressed="false" class="red-bg option">
-      <input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.b) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option b" aria-pressed="false" class="orange-bg option">
+      <input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.a) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option a" aria-pressed="false" class="red-bg option arrowNav">
+      <input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.b) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option b" aria-pressed="false" class="orange-bg option arrowNav">
     </form>
     <form class="options">
-      <input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.c) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option c" aria-pressed="false" class="green-bg option">
-      <input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.d) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option d" aria-pressed="false" class="blue-bg option">
+      <input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.c) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option c" aria-pressed="false" class="green-bg option arrowNav">
+      <input type="button" style="border: none; height: 133px; background-image: url(images/' + (STORE.questions[qN].answers.d) + '); background-repeat: no-repeat; background-size: 100% 100%; margin: 0; padding: 0;" alt="option d" aria-pressed="false" class="blue-bg option arrowNav">
     </form>
 
-    <button type="button" id="submit" class="submit">Submit</button>
-    <button type="button" id="next" class="next">Next</button>
+    <button type="button" id="submit" class="submit arrowNav">Submit</button>
+    <button type="button" id="next" class="next arrowNav">Next</button>
 
   </div>
   
@@ -130,7 +130,7 @@ function generateTemplate(qN) {
 }
 
 function finalScoreTemplate() {
-  return '<div class="main-parent"><img src="https://media.giphy.com/media/l4KhUsTvaxtBP3fzi/giphy.gif" alt="animated kickflip over trash can"><div class="box"><p><b>Your final score is: ' + (STORE.score) + '/5</b></p><button type="button" id="start" class="startQuiz">Start New Quiz</button></div></div>';
+  return '<div class="main-parent"><img src="https://media.giphy.com/media/l4KhUsTvaxtBP3fzi/giphy.gif" alt="animated kickflip over trash can"><div class="box"><p><b>Your final score is: ' + (STORE.score) + '/5</b></p><button type="button" id="start" class="startQuiz arrowNav">Start New Quiz</button></div></div>';
 
   /*
 
@@ -140,7 +140,7 @@ function finalScoreTemplate() {
 
   <div class="box">
     <p><b>Your final score is: ' + (STORE.score) + '/5</b></p>
-    <button type="button" id="start" class="startQuiz">Start New Quiz</button>
+    <button type="button" id="start" class="startQuiz arrowNav">Start New Quiz</button>
   </div>
   
   </div>'
@@ -164,7 +164,7 @@ function render() {
 }
 
 function startScreen() {
-  $('#main').html('<div class="main-parent"><img src="https://media.giphy.com/media/xT0GqpNlINWjnQEXpC/giphy.gif" alt="slow motion skateboarding trick"><div class="box"><p>This quiz will test your knowledge of skateboarding tricks.</p><button type="button" id="start" class="startQuiz">Start Quiz</button></div></div><div class="main-parent"></div>');
+  $('#main').html('<div class="main-parent"><img src="https://media.giphy.com/media/xT0GqpNlINWjnQEXpC/giphy.gif" alt="slow motion skateboarding trick"><div class="box"><p>This quiz will test your knowledge of skateboarding tricks.</p><button type="button" id="start" class="startQuiz arrowNav">Start Quiz</button></div></div><div class="main-parent"></div>');
   
   /*
 
@@ -172,7 +172,7 @@ function startScreen() {
       <img src="https://media.giphy.com/media/xT0GqpNlINWjnQEXpC/giphy.gif" alt="slow motion skateboarding trick">
       <div class="box">
         <p>This quiz will test your knowledge of skateboarding tricks.</p>
-        <button type="button" id="start" class="startQuiz">Start Quiz</button>
+        <button type="button" id="start" class="startQuiz arrowNav">Start Quiz</button>
       </div>
     </div>
 
@@ -229,6 +229,50 @@ function nextButton() {
   });
 }
 
+
+function arrowKeyNavigation() {
+  $('body').on('keydown', function(event) {
+    var currentOption = document.activeElement;
+
+    // left
+    if (event.which == 37) {
+      event.preventDefault();
+      console.log('left pressed');
+      $(currentOption).prev('.option').focus();
+    }
+
+    // up
+    if (event.which == 38 && $(currentOption).attr('alt') == 'option c') {
+      event.preventDefault();
+      console.log('up pressed');
+      $('.box').find('.option').first().focus();
+    } else if (event.which == 38 && $(currentOption).attr('alt') == 'option d') {
+      event.preventDefault();
+      console.log('up pressed');
+      $('.box').find('.option').first().next().focus();
+    }
+
+    // right  
+    if (event.which == 39) {
+      event.preventDefault();
+      console.log('right pressed');
+      $(currentOption).next('.option').focus();
+    }
+
+    // down
+    if (event.which == 40 && $(currentOption).attr('alt') == 'option a') {
+      event.preventDefault();
+      console.log('down pressed');
+      $('.box').find('.option').last().prev().focus();
+    } else if (event.which == 40 && $(currentOption).attr('alt') == 'option b') {
+      event.preventDefault();
+      console.log('down pressed');
+      $('.box').find('.option').last(). focus();
+    }
+  });
+}
+
+
 function handleOptionClicks() {
   $('.option').attr('aria-pressed', false)
   $('.main-parent').on('click', '.option', function(event) {
@@ -244,6 +288,7 @@ function oneFunctionToRunThemAll() {
   handleOptionClicks();
   submitButton();
   nextButton();
+  arrowKeyNavigation();
 }
 
 $(oneFunctionToRunThemAll);
